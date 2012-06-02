cfe9e533458a77d8228bd8c09c6693e050d09527
Stefan Plantikow <stefanp@moviepilot.com>
2012-06-02 21:50:53 +0200

    Titulator data fetching works now

--- lib/titulator/fetch.rb
+++ lib/titulator/fetch.rb
@@ -35,25 +35,63 @@ module Titulator
       end
     end
 
-    def osdb_find_set(lang, imdb_id)
-      osdb_select_set osdb_search(lang, imdb_id)
+    # Example:
+    #   f.osdb_subtitles(:en, 499549)
+    #   f.osdb_subtitles(:en, 133093)
+    #
+    def osdb_subtitles(lang, imdb_id, start = nil, stop = nil)
+      start       = MilliTime.parse(start) if start.is_a?(String)
+      start       = MilliTime.new(start) if start.is_a?(Fixnum)
+      stop        = MilliTime.parse(stop) if stop.is_a?(String)
+      stop        = MilliTime.new(stop) if stop.is_a?(Fixnum)
+      cands       = osdb_select_set osdb_search(lang, imdb_id)
+      subs        = osdb_load_subtitles cands
+      titles      = osdb_join_subtitles subs
+      return [] if titles.size == 0
+      start_index = if start
+        then titles.bin_index(start, :asc) { |a, b| a <=> b.start }
+        else 0 end
+      return [] if start_index < 0
+      stop_index  = if stop
+        then [start_index, titles.bin_index(stop, :asc) { |a, b| a <=> b.stop } - 1].max
+        else -1 end
+      stop_index  = titles.size-1 if stop_index < 0
+      raise ArgumentError, 'start > stop' if start_index > stop_index
+      titles[start_index..stop_index]
     end
 
-    # Example:
-    #   cands=f.osdb_find_set(:en, 499549)
-    #
+    private
+
     def osdb_select_set(cands)
       raise ArgumentError, 'Invalid candidate set' if cands.size == 0
       cands[0..cands.first.raw_data['SubSumCD'].to_i - 1]
     end
 
     def osdb_load_subtitles(cands)
-      cands.map do |cand|
+      subs = cands.map do |cand|
         content = Net::HTTP.get_response(cand.url).body
         gz_body = Zlib::GzipReader.new StringIO.new(content), external_encoding: content.encoding
         Parser.parse cand.format.chomp.downcase.to_sym, gz_body.read
       end
     end
-  end
 
+    def osdb_join_subtitles(subs)
+      result = []
+      shift  = MilliTime::ZERO
+      prev   = MilliTime::ZERO
+      subs.each do |captions|
+        captions.each do |caption|
+          caption = caption + shift
+          if (caption.start <=> prev) == -1
+            shift   = prev
+            caption = caption + prev - shift
+          end
+          result << caption
+          prev    = caption.start
+        end
+      end
+      result
+    end
+  end
 end
+
